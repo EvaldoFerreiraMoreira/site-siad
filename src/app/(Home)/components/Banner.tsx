8c7322281a490aaf7608747ce274bd3a2a8d62df
GabrielUlick <gabrieludb@hotmail.com>
2023-11-22 10:12:27 -0300

    feat: create first component in Homepage

---
+++ src/app/(Home)/components/Banner.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+const Banner = () => {
+    return (
+        <div className="text-[#A7A7A7]">
+            <div className="flex w-56 font-semibold text-2xl">
+                <h1>Lorem <span className="text-[#3D5685]">Ipsum </span>is simply <span className="text-[#3D5685]">dummy </span>text of <span className="text-[#3D5685]">the </span> printing </h1>
+            </div>
+            <div className="mt-6 text-sm text-justify">
+                <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type  </p>
+            </div>
+            <div className="flex mt-10 h-56 justify-center">
+            <Image
+            src="/PC-01.png"
+            alt="Compiuter"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-auto max-w-[100%] max-h-[90%]"
+            style={{
+              objectFit: "contain",
+            }}
+          />
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,9 +1,10 @@
+import Banner from "./components/Banner";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <div className="border h-96">
-
+    <div className="flex flex-col pt-24">
+      <div className="flex w-full p-10">
+        <Banner />
       </div>
       <div className="border bg-[#3D5685] h-96">
 

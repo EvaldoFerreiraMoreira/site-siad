a07257d4af67ef2b82858e40fdb0ab4122523a30
Gabriel Ulick <gabrieludb@hotmail.com>
2023-11-22 19:45:21 -0300

    fix: separate Navigation menu to Navigation Menu Mobile and Navigation Menu Web

--- src/components/ui/componentsMobile/NavigationMenuMobile.tsx
+++ src/components/ui/componentsMobile/NavigationMenuMobile.tsx
@@ -11,54 +11,15 @@ import {
     NavigationMenuLink,
     NavigationMenuList,
     NavigationMenuTrigger,
-    navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 import { Briefcase } from "lucide-react"
 
-const components: { title: string; href: string; description: string }[] = [
-    {
-        title: "Alert Dialog",
-        href: "/docs/primitives/alert-dialog",
-        description:
-            "A modal dialog that interrupts the user with important content and expects a response.",
-    },
-    {
-        title: "Hover Card",
-        href: "/docs/primitives/hover-card",
-        description:
-            "For sighted users to preview content available behind a link.",
-    },
-    {
-        title: "Progress",
-        href: "/docs/primitives/progress",
-        description:
-            "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-    },
-    {
-        title: "Scroll-area",
-        href: "/docs/primitives/scroll-area",
-        description: "Visually or semantically separates content.",
-    },
-    {
-        title: "Tabs",
-        href: "/docs/primitives/tabs",
-        description:
-            "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-    },
-    {
-        title: "Tooltip",
-        href: "/docs/primitives/tooltip",
-        description:
-            "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-    },
-]
-
-export function NavigationMenuSegmento() {
+export function NavigationMenuSegmentoMobile() {
     return (
         <NavigationMenu>
             <NavigationMenuList>
                 <NavigationMenuItem>
-                    <NavigationMenuTrigger className="w-full justify-start w-60 gap-2 border">
+                    <NavigationMenuTrigger className="justify-start w-60 gap-2 border">
                         <Briefcase size={16} className="left-10"/>
                         Segmentos
                     </NavigationMenuTrigger>

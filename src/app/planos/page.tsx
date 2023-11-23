4465442379d69fbac566c2dd7988343b322f8d3c
GabrielUlick <gabrieludb@hotmail.com>
2023-11-23 10:57:37 -0300

    feat: Banner Planos on page

--- src/app/planos/page.tsx
+++ src/app/planos/page.tsx
@@ -1,11 +1,36 @@
+import BannerPlanos from "./components/BannerPlanos";
+import CarrosselPlanos from "./components/CarrosselPlanos";
 
 
 const Planos = () => {
-    return ( 
-        <div>
-            <p>PLANOS</p>
+    return (
+        <div className="flex flex-col pt-24">
+            <div className=" flex w-full p-10 bg-[#f1f4f9] h-96 ">
+                <BannerPlanos />
+            </div>
+            <div className=" flex w-full p-10 bg-[#f1f4f9] h-96 ">
+
+            </div>
+            <div className="border bg-[#3D5685]">
+                <CarrosselPlanos />
+            </div>
+            <div className=" flex w-full p-10  bg-[#f1f4f9] h-96 ">
+
+            </div>
+            <div className=" flex w-full p-10 bg-[#f1f4f9] h-96 ">
+
+            </div>
+            <div className=" flex w-full p-10 bg-[#f1f4f9] h-96 ">
+
+            </div>
+            <div className=" flex w-full p-10 bg-[#f1f4f9] h-96 ">
+
+            </div>
+            <div className="flex w-full p-10 bg-[#3D5685] h-96 ">
+
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Planos;

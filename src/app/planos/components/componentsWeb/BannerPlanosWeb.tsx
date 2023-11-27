58f9a655a35138b3192f2dc5393c4b63a6ff1d2f
GabrielUlick <gabrieludb@hotmail.com>
2023-11-27 12:04:08 -0300

    feat: carrosselplanos web

---
+++ src/app/planos/components/componentsWeb/BannerPlanosWeb.tsx
@@ -0,0 +1,16 @@
+const BannerPlanosWeb = () => {
+    return (
+        <div className="flex flex-col items-center text-center w-full">
+            <div className="flex flex-col text-[#A7A7A7] items-center text-center">
+                <div className="flex w-2/3 font-semibold text-3xl">
+                    <h1>Lorem <span className="text-[#3D5685]">Ipsum </span>is simply <span className="text-[#3D5685]">dummy </span>text of <span className="text-[#3D5685]">the </span> printing </h1>
+                </div>
+                <div className="mt-6 text-sm">
+                    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default BannerPlanosWeb;

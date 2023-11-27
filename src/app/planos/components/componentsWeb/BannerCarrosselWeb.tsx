0fe67eecf6438d3f9cc054ef706373497f8d0ea8
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2023-11-27 12:00:49 -0300

    feax: update components pages web

--- src/app/planos/components/componentsWeb/BannerCarrosselWeb.tsx
+++ src/app/planos/components/componentsWeb/BannerCarrosselWeb.tsx
@@ -16,71 +16,35 @@ const BannerCarrosselWeb = () => {
     const caract = ">";
     const items = [
         <div className="item" data-value="1" key={0}>
-            <div className="flex items-center text-center mt-9">
-                <h1 className="text-[#333333]  font-bold text-4xl p-6">
-                    Lorem Ipsum is simply dummy text of the printing
-                </h1>
-            </div>
-            <div className="flex mt-1 ">
-                <p className="text-[#A7A7A7]  font-semibold  text-md text-justify p-6">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry s standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type.
-                </p>
-            </div>
-            <div className='flex mt-5 justify-center gap-2 pb-5 '>
-                <Image
-                    src={"/PC-01.png"}
-                    alt="Imagem"
-                    width={0}
-                    height={0}
-                    sizes="auto"
-                    className="h-56 w-auto max-w-[100%] max-h-[100%]"
-                    style={{
-                        objectFit: "contain",
-                    }}
-                />
-            </div>
-            <div className="flex mt-1 justify-center">
-                <p className="text-[#3D5685]  font-bold text-sm text-center hover:text-[#333333] ">
-                    Lorem Ipsum is simply dummy text of
-                </p>
+            <div className="flex text-[#A7A7A7] mx-64">
+                <div className="flex flex-col justify-center">
+                    <div className="flex font-bold text-4xl">
+                        <h1 className="text-[#000000]  font-bold text-4xl">
+                            Lorem Ipsum is simply dummy text of the printing
+                        </h1>
+                    </div>
+                    <div className="mt-6 w-2/3 text-sm font-semibold text-justify">
+                        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry s standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex mt-10 justify-center">
+                    <Image
+                        src="/PC-01.png"
+                        alt="Compiuter"
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        className="h-auto w-auto max-w-[100%] max-h-[100%]"
+                        style={{
+                            objectFit: "contain",
+                        }}
+                    />
+                </div>
             </div>
         </div>,
-        <div className="item" data-value="2" key={1}>
-            <div className="flex items-center text-center mt-9">
-                <h1 className="text-[#333333]  font-bold text-4xl p-6">
-                    Lorem Ipsum is simply dummy text of the printing
-                </h1>
-            </div>
-            <div className="flex mt-1 ">
-                <p className="text-[#A7A7A7]  font-semibold  text-md text-justify p-6">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry s standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type.
-                </p>
-            </div>
-            <div className='flex mt-5 justify-center gap-2 pb-5 '>
-                <Image
-                    src={"/PC-01.png"}
-                    alt="Imagem"
-                    width={0}
-                    height={0}
-                    sizes="auto"
-                    className="h-56 w-auto max-w-[100%] max-h-[100%]"
-                    style={{
-                        objectFit: "contain",
-                    }}
-                />
-            </div>
-            <div className="flex mt-1 justify-center">
-                <p className="text-[#3D5685]  font-bold text-sm text-center hover:text-[#333333] ">
-                    Lorem Ipsum is simply dummy text of
-                </p>
-            </div>
-        </div>,
-        
-
 
     ];
 
@@ -94,7 +58,7 @@ const BannerCarrosselWeb = () => {
 
 
     return (
-        <div className='flex mx-64'>
+        <div className=''>
             <AliceCarousel
                 mouseTracking
                 disableButtonsControls
@@ -103,6 +67,7 @@ const BannerCarrosselWeb = () => {
                 onSlideChange={onSlideChange}
                 onSlideChanged={onSlideChanged}
             />
+
         </div>
     );
 }

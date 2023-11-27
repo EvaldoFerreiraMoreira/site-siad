f6124b18cf6a1b0124e4e45e56aca2279da843d0
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2023-11-27 09:51:41 -0300

    feat: creates componets Web

---
+++ src/app/(Home)/components/componentsWeb/BannerConsultantWeb.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+
+const BannerConsultantWeb = () => {
+    return ( 
+        <div className="mx-64">
+            <div className=" flex font-semibold text-4xl">
+                <h1 className="text-center font-bold text-[#3D5685]">Como a SIAD pode ajudar o seu negócio?</h1>
+            </div>
+            <div className=" flex  justify-center mt-10 text-sm text-center">
+                <p className="text-center font-semibold text-[#3D5685]">Estamos prontos para ajudar.</p>
+            </div>
+            <div className="flex mt-10 justify-center">
+                <Button 
+                className="bg-[#FFFFFF] border w-64 hover:bg-[#3D5685]">
+                    <p className="text-center font-semibold text-[#000000] hover:text-[#FFFFFF]">Falar com consultor</p>
+                </Button>
+            </div>
+        </div>
+     );
+     
+}
+ 
+export default BannerConsultantWeb;
---
+++ src/app/(Home)/components/componentsWeb/MobilePresentationWeb.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import { Smartphone } from "lucide-react";
+import Image from "next/image";
+
+
+const MobilePresentationWeb = () => {
+    return (
+        <div className=" flex text-[#A7A7A7] mx-64 ">
+            <div className="flex flex-col justify-center ">
+                <div className="flex w-72 font-bold  text-4xl">
+                    <h1 className="text-[#3D5685]">Gestor Mobile</h1>
+                </div>
+                <div className="mt-6 w-2/3 font-semibold text-sm text-justify">
+                    <span className="text-[#3D5685]">Confira a nossa solução mobile com os principais resultados da sua empresa</span>
+                </div>
+                <div className="flex flex-col  gap-2 mt-5 text-[#3D5685] text-xs" >
+                    <ul className="w-full justify-start gap-2" >
+                        <li className="flex gap-1 ">
+                            <Smartphone size={25} />
+                            <p> Melhor horário das vendas</p>
+                        </li>
+                        <li className="flex gap-1 mt-2">
+                            <Smartphone size={25} />
+                            <p>mês a mês</p>
+                        </li>
+                        <li className="flex gap-1 mt-2">
+                            <Smartphone size={25} />
+                            <p>pranking de vendas</p>
+                        </li>
+                        <li className="flex gap-1 mt-2" >
+                            <Smartphone size={25} />
+                            <p>formas de pagamentos e vendedores</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="flex mt-8  ml-3">
+                    <Button className="bg-[#26B547]">
+                        <p>Experimente</p>
+                    </Button>
+                </div>
+            </div>
+
+            <div className="pr-20">
+                <div className="flex mt-9 h-72 justify-center">
+                    <Image
+                        src="/Iphone-mobile.png"
+                        alt="SMARTPHONE"
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        className="h-auto w-auto max-w-[100%] max-h-[100%]"
+                        style={{
+                            objectFit: "cover",
+                        }}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MobilePresentationWeb;

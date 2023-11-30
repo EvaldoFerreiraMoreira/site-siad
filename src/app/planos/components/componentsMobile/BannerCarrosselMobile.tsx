96e450f5b002b4cd4f011bdb16f1d6bc7b0ec144
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2023-11-30 08:32:35 -0300

    feat: text and componebts BannerCarrosel

--- src/app/planos/components/componentsMobile/BannerCarrosselMobile.tsx
+++ src/app/planos/components/componentsMobile/BannerCarrosselMobile.tsx
@@ -48,14 +48,89 @@ const BannerCarrosselMobile = () => {
             </div>
             <div className="flex mt-1 ">
                 <p className="text-[#A7A7A7]  font-semibold  text-md text-justify p-6">
-                Agilize o processo de entrada e fechamento de notas com o uso do nosso manifestador de notas 
-                fiscais de entrada e manutenção de itens, sem a necessidade de redigitar a nota ou 
-                os itens para acrescentá-los ao estoque.
+                    Agilize o processo de entrada e fechamento de notas com o uso do nosso manifestador de notas
+                    fiscais de entrada e manutenção de itens, sem a necessidade de redigitar a nota ou
+                    os itens para acrescentá-los ao estoque.
                 </p>
             </div>
             <div className='flex mt-5 justify-center gap-2 pb-5 '>
                 <Image
                     src={"/IMG-06.jpg"}
+                    alt="Imagem"
+                    width={0}
+                    height={0}
+                    sizes="auto"
+                    className="h-56 w-auto max-w-[100%] max-h-[100%]"
+                    style={{
+                        objectFit: "contain",
+                    }}
+                />
+            </div>
+        </div>,
+        <div className="item" data-value="2" key={2}>
+            <div className="flex items-center text-center mt-9">
+                <h1 className="text-[#333333]  font-bold text-xl p-6">
+                    Ordem de serviço
+                </h1>
+            </div>
+            <div className="flex mt-1 ">
+                <p className="text-[#A7A7A7]  font-semibold  text-md text-justify p-6">
+                    Gerencie seus serviços prestados pela empresa com nosso módulo de Ordem de Serviço e gere comprovantes dos mesmos.
+                </p>
+            </div>
+            <div className='flex mt-5 justify-center gap-2 pb-5 '>
+                <Image
+                    src={"/IMG-07.jpg"}
+                    alt="Imagem"
+                    width={0}
+                    height={0}
+                    sizes="auto"
+                    className="h-56 w-auto max-w-[100%] max-h-[100%]"
+                    style={{
+                        objectFit: "contain",
+                    }}
+                />
+            </div>
+        </div>,
+        <div className="item" data-value="2" key={3}>
+            <div className="flex items-center text-center mt-9">
+                <h1 className="text-[#333333]  font-bold text-xl p-6">
+                    Amplie seu empreendimento com uma gestão financeira estruturada
+                </h1>
+            </div>
+            <div className="flex mt-1 ">
+                <p className="text-[#A7A7A7]  font-semibold  text-md text-justify p-6">
+                    Um sistema centralizado de informações que integra procedimentos burocráticos com análises de saúde do negócio, proporcionando uma previsão mais precisa de lucros e uma tomada de decisão mais transparente. Oferece backups automáticos recorrentes e otimizados, garantindo máxima segurança dos dados.
+                </p>
+            </div>
+            <div className='flex mt-5 justify-center gap-2 pb-5 '>
+                <Image
+                    src={"/IMG-08.jpg"}
+                    alt="Imagem"
+                    width={0}
+                    height={0}
+                    sizes="auto"
+                    className="h-56 w-auto max-w-[100%] max-h-[100%]"
+                    style={{
+                        objectFit: "contain",
+                    }}
+                />
+            </div>
+        </div>,
+        <div className="item" data-value="2" key={4}>
+            <div className="flex items-center text-center mt-9">
+                <h1 className="text-[#333333]  font-bold text-xl p-6">
+                Envie suas Notas Fiscais de Forma Mais Simples e Prática
+                </h1>
+            </div>
+            <div className="flex mt-1 ">
+                <p className="text-[#A7A7A7]  font-semibold  text-md text-justify p-6">
+                Ao administrar seu empreendimento com o SIAD, você economiza tempo e elimina retrabalhos ao gerar suas notas fiscais diretamente a partir das vendas, eliminando a necessidade de reintroduzir produtos, serviços e informações dos clientes nos portais governamentais.
+                </p>
+            </div>
+            <div className='flex mt-5 justify-center gap-2 pb-5 '>
+                <Image
+                    src={"/IMG-09.jpg"}
                     alt="Imagem"
                     width={0}
                     height={0}
--- src/app/planos/components/componentsWeb/BannerCarrosselWeb.tsx
+++ src/app/planos/components/componentsWeb/BannerCarrosselWeb.tsx
@@ -12,7 +12,7 @@ const BannerCarrosselWeb = () => {
         568: { items: 1 },
         1024: { items: 1 },
     };
-    
+
     const items = [
         <div className="item" data-value="1" key={0}>
             <div className="flex text-[#A7A7A7] mx-64">
@@ -43,7 +43,7 @@ const BannerCarrosselWeb = () => {
                 </div>
             </div>
         </div>,
-        <div className="item" data-value="1" key={0}>
+        <div className="item" data-value="1" key={1}>
             <div className="flex text-[#A7A7A7] mx-64">
                 <div className="flex flex-col justify-center">
                     <div className="flex font-bold text-4xl">
@@ -52,7 +52,7 @@ const BannerCarrosselWeb = () => {
                         </h1>
                     </div>
                     <div className="mt-6 w-2/3 text-sm font-semibold text-justify">
-                        <p> 
+                        <p>
                             Agilize o processo de entrada e fechamento de notas com o uso do nosso manifestador de notas
                             fiscais de entrada e manutenção de itens, sem a necessidade de redigitar a nota ou
                             os itens para acrescentá-los ao estoque.
@@ -74,6 +74,94 @@ const BannerCarrosselWeb = () => {
                 </div>
             </div>
         </div>,
+        <div className="item" data-value="1" key={2}>
+            <div className="flex text-[#A7A7A7] mx-64">
+                <div className="flex flex-col justify-center">
+                    <div className="flex font-bold text-4xl">
+                        <h1 className="text-[#000000]  font-bold text-4xl">
+                            Ordem de serviço
+                        </h1>
+                    </div>
+                    <div className="mt-6 w-2/3 text-sm font-semibold text-justify">
+                        <p>
+                            Gerencie seus serviços prestados pela empresa com nosso módulo de Ordem de Serviço e gere comprovantes dos mesmos.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex mt-10 justify-center">
+                    <Image
+                        src="/IMG-07.jpg"
+                        alt="Compiuter"
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        className="h-auto w-auto max-w-[100%] max-h-[100%]"
+                        style={{
+                            objectFit: "contain",
+                        }}
+                    />
+                </div>
+            </div>
+        </div>,
+        <div className="item" data-value="1" key={3}>
+            <div className="flex text-[#A7A7A7] mx-64">
+                <div className="flex flex-col justify-center">
+                    <div className="flex font-bold text-4xl">
+                        <h1 className="text-[#000000]  font-bold text-4xl">
+                            Amplie seu empreendimento com uma gestão financeira estruturada
+                        </h1>
+                    </div>
+                    <div className="mt-6 w-2/3 text-sm font-semibold text-justify">
+                        <p>
+                            Um sistema centralizado de informações que integra procedimentos burocráticos com análises de saúde do negócio, proporcionando uma previsão mais precisa de lucros e uma tomada de decisão mais transparente. Oferece backups automáticos recorrentes e otimizados, garantindo máxima segurança dos dados.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex mt-10 justify-center">
+                    <Image
+                        src="/IMG-08.jpg"
+                        alt="Compiuter"
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        className="h-auto w-auto max-w-[100%] max-h-[100%]"
+                        style={{
+                            objectFit: "contain",
+                        }}
+                    />
+                </div>
+            </div>
+        </div>,
+        <div className="item" data-value="1" key={4}>
+            <div className="flex text-[#A7A7A7] mx-64">
+                <div className="flex flex-col justify-center">
+                    <div className="flex font-bold text-4xl">
+                        <h1 className="text-[#000000]  font-bold text-4xl">
+                            Envie suas Notas Fiscais de Forma Mais Simples e Prática
+                        </h1>
+                    </div>
+                    <div className="mt-6 w-2/3 text-sm font-semibold text-justify">
+                        <p>
+                            Ao administrar seu empreendimento com o SIAD, você economiza tempo e elimina retrabalhos ao gerar suas notas fiscais diretamente a partir das vendas, eliminando a necessidade de reintroduzir produtos, serviços e informações dos clientes nos portais governamentais.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex mt-10 justify-center">
+                    <Image
+                        src="/IMG-09.jpg"
+                        alt="Compiuter"
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        className="h-auto w-auto max-w-[100%] max-h-[100%]"
+                        style={{
+                            objectFit: "contain",
+                        }}
+                    />
+                </div>
+            </div>
+        </div>,
+
 
     ];
 

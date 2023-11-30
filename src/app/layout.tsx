22ddf066366380082793ee1877fc35ceb2aad480
GabrielUlick <gabrieludb@hotmail.com>
2023-11-30 10:31:49 -0300

    fix: weight font roboto on layout.tsx and remove import font Inter unused

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from 'next'
-import { Inter, Roboto } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/ui/Header'
 import Footer from '@/components/ui/Footer'
 import FixedBar from '@/components/ui/FixedBar'
 
-const inter = Inter({ subsets: ['latin'] })
 const roboto = Roboto({
   subsets: ['latin'],
-  weight: '900'
+  weight: '500'
 })
 
 export const metadata: Metadata = {

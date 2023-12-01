af21509a976e3641b89350920aca784efd2c8ce8
IsabellaPassosCorrea <isabellapassoscorrea@gmail.com>
2023-12-01 09:41:41 -0300

    fix: telefone contato mobile

--- src/app/planos/components/componentsMobile/BannerContatoMobile.tsx
+++ src/app/planos/components/componentsMobile/BannerContatoMobile.tsx
@@ -13,14 +13,16 @@ const BannerContatoMobile = () => {
                         <FaWhatsapp size={40} style={{ color: "#3D5685" }} />
                     </p>
                 </Link>
-                <h1 className="text-center font-bold text-[#FFFFFF] text-sm" >mande um whastapp</h1>
+                <h1 className="text-center font-normal text-[#FFFFFF] text-sm" >mande um whastapp</h1>
             </div>
 
             <div className=" text-center  bg-white w-24 h-24 rounded-full shadow-lg shadow-[#23314C] gap-2">
                 <p className="flex gap-1 justify-center mt-7 mb-10 ">
                     <Phone size={40} style={{ color: "#3D5685" }} />
                 </p>
-                <h1 className="text-center font-bold text-[#FFFFFF] text-sm" >(33) 32673797</h1>
+                <Link href={"tel:32673797"}>
+                <h1 className="text-center text-[#FFFFFF] font-medium text-sm" >(33) 32673797</h1>
+                </Link>
             </div>
 
         </div>

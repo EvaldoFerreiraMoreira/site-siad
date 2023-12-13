f4a82700c06f7d74b6cdcca6ba41d2ed0fe06bd5
GabrielUlick <gabrieludb@hotmail.com>
2023-12-13 08:30:00 -0300

    fix: link whatsapp bannerContato

--- src/app/planos/components/componentsWeb/BannerContatoWeb.tsx
+++ src/app/planos/components/componentsWeb/BannerContatoWeb.tsx
@@ -7,14 +7,14 @@ const BannerContatoWeb = () => {
     return (
         <div className="flex  items-center justify-center text-center gap-32 h-36 mx-64  w-[80%] ">
             <div >
-                <div className=" text-center ml-5  border  border-white bg-white w-24 h-24 rounded-full shadow-lg shadow-[#23314C] gap-2">
-                    <Link href="https://api.whatsapp.com/send?phone=553398056488&text=Ol%C3%A1%2C%20vim%20pelo%20Site%20da%20Siad">
+                <Link href="https://api.whatsapp.com/send?phone=553398056488&text=Ol%C3%A1%2C%20vim%20pelo%20Site%20da%20Siad">
+                    <div className=" text-center ml-5  border  border-white bg-white w-24 h-24 rounded-full shadow-lg shadow-[#23314C] gap-2">
                         <p className="flex gap-1 justify-center mt-7 mb-10 ">
-                            <FaWhatsapp size={40} style={{ color: "#3D5685" }} />
+                            <FaWhatsapp size={41} style={{ color: "#3D5685" }} />
                         </p>
-                    </Link>
-                </div>
-                <h1 className="text-center  mt-2 font-medium  text-[#FFFFFF] text-sm" >Mande um whastapp</h1>
+                    </div>
+                    <h1 className="text-center  mt-2 font-medium  text-[#FFFFFF] text-sm" >Mande um whastapp</h1>
+                </Link>
             </div>
             <div className="flex flex-col items-center justify-center">
                 <div className=" text-center ml-5  border  border-white bg-white w-24 h-24 rounded-full shadow-lg shadow-[#23314C] gap-2">

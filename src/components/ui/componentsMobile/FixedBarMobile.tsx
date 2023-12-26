49a477cb8022323a065e2fa5b6293a2b17344d93
IsabellaPassosCorrea <isabellapassoscorrea@gmail.com>
2023-12-26 15:12:54 -0300

    fix: criação e modificação d da barra fixa junto do formulário mobile

--- src/components/ui/componentsMobile/FixedBarMobile.tsx
+++ src/components/ui/componentsMobile/FixedBarMobile.tsx
@@ -1,26 +1,34 @@
 import Link from "next/link";
 import { FaWhatsapp } from "react-icons/fa";
+import FormularioMobile from "./FormularioMobile";
+import FormularioModificadoMobile from "./FormularioModificadoMobile";
 
 const FixedBarMobile = () => {
     return (
         <div className="fixed bg-[#5576B3] container shadow-md mx-auto p-5 bottom-0 h-[70px] flex items-center text-white z-20">
-            <div className="flex items-center ">
+            <div className="flex gap-5">
+                
+            <div className="flex   ">
             <Link href="https://api.whatsapp.com/send?phone=553398056488&text=Ol%C3%A1%2C%20vim%20pelo%20Site%20da%20Siad">
                 <FaWhatsapp size={35} />
             </Link>
-            </div>
             <Link href="https://api.whatsapp.com/send?phone=553398056488&text=Ol%C3%A1%2C%20vim%20pelo%20Site%20da%20Siad">
-                <div>
-                    <p className="text-sm w-36 text-center font-semibold">
-                        Fale com a gente no
-                        Whatsapp
+                <div className="flex flex-col">
+                    <p className="text-sm w-24 text-center font-semibold">
+                        Fale no
                     </p>
+                    <p className="text-sm w-24 text-center font-semibold">Whatsapp</p>
                 </div>
             </Link>
+            </div>
+            <div className="flex ">
+            <FormularioModificadoMobile/>
+            </div>
             <div className="flex w-2 h-4 left-full ">
                 <script src="//code.jivosite.com/widget/pasrKoKcWR" async></script>
             </div>
         </div>
+            </div>
     );
 }
 
